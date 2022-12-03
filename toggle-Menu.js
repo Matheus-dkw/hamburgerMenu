2c2ed95ebe476876cc44c568791ae57c2a6c4203
Matheus-dkw <matheus-fs@outlook.com.br>
2022-12-03 12:38:38 -0300

    hamburgerMenu

---
+++ toggle-Menu.js
@@ -0,0 +1,13 @@
+
+
+const btn = document.querySelector('.hamburger-lines')
+
+const showMenu = document.querySelector('.toggle-Menu') 
+
+
+
+btn.addEventListener('click', function(){
+    showMenu.classList.toggle('hide-menu')
+    
+})
+
